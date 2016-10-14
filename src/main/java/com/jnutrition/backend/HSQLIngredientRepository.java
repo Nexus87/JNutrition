ebf1c33c45851323ae90e9523145c685127c0729
nexus87 <nexxuz0@googlemail.com>
2016-10-14 16:03:32 +0200

    Using jpa instead of Hibernate api

--- src/main/java/com/jnutrition/backend/HSQLIngredientRepository.java
+++ src/main/java/com/jnutrition/backend/HSQLIngredientRepository.java
@@ -3,38 +3,33 @@ package com.jnutrition.backend;
 import com.jnutrition.model.Ingredient;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import org.hibernate.Criteria;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.criterion.MatchMode;
-import org.hibernate.criterion.Restrictions;
-import org.hibernate.service.ServiceRegistry;
 import org.springframework.beans.factory.InitializingBean;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.List;
 
 @Component
 public class HSQLIngredientRepository implements IngredientRepository, InitializingBean{
-    private String cfgPath;
-    private SessionFactory sessionFactory;
     private ObservableList<Ingredient> ingredients = FXCollections.observableArrayList();
-
-    @Value(value = "${cfg.hibernate}")
-    public void setCfgPath(String cfgPath) {
-        this.cfgPath = cfgPath;
-    }
+    private EntityManagerFactory entityManagerFactory;
 
     @Override
     public ObservableList<Ingredient> getAllIngredients() {
         ingredients.clear();
 
-        Session session = sessionFactory.getCurrentSession();
-        session.beginTransaction();
-        Criteria criteria = session.createCriteria(Ingredient.class);
-        ingredients.addAll(criteria.list());
-        session.getTransaction().commit();
-        session.close();
+        EntityManager em = entityManagerFactory.createEntityManager();
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<Ingredient> c = cb.createQuery(Ingredient.class);
+        Root<Ingredient> i = c.from(Ingredient.class);
+
+        c.select(i);
+        List resultList = em.createQuery(c).getResultList();
+        ingredients.addAll(resultList);
         return ingredients;
     }
 
@@ -42,61 +37,29 @@ public class HSQLIngredientRepository implements IngredientRepository, Initializ
     public void setNameFilter(String name) {
         ingredients.clear();
 
-        Session session = sessionFactory.getCurrentSession();
-        session.beginTransaction();
-        Criteria criteria = session.createCriteria(Ingredient.class);
-        criteria.add(Restrictions.like("name", name, MatchMode.ANYWHERE).ignoreCase());
-        ingredients.addAll(criteria.list());
-        session.getTransaction().commit();
-        session.close();
+        EntityManager em = entityManagerFactory.createEntityManager();
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<Ingredient> c = cb.createQuery(Ingredient.class);
+        Root<Ingredient> i = c.from(Ingredient.class);
+        c.where(cb.like(cb.lower(i.get("name")), "%" + name.toLowerCase() + "%"));
+
+        List resultList = em.createQuery(c).getResultList();
+
+        ingredients.addAll(resultList);
     }
 
     @Override
     public Object getIngredientByName(String name) {
-        Session session = sessionFactory.getCurrentSession();
-        session.beginTransaction();
-        Criteria criteria = sessionFactory.getCurrentSession().createCriteria(Ingredient.class);
-        criteria.add(Restrictions.eq("name", name));
-        Object result = criteria.list().stream().findFirst().orElse(null);
-        session.getTransaction().commit();
-        session.close();
-        return result;
+        return new Object();
     }
 
     @Override
     public void close() {
-        sessionFactory.close();
+        //entityManagerFactory.close();
     }
 
     @Override
     public void afterPropertiesSet() throws Exception {
-        try {
-            // Create the SessionFactory from hibernate.cfg.xml
-            org.hibernate.cfg.Configuration configuration = new org.hibernate.cfg.Configuration();
-            configuration.configure(cfgPath);
-            configuration.addAnnotatedClass(Ingredient.class);
-            System.out.println("Hibernate Annotation Configuration loaded");
-
-            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
-            System.out.println("Hibernate Annotation serviceRegistry created");
-
-            sessionFactory = configuration.buildSessionFactory(serviceRegistry);
-        }
-        catch (Throwable ex) {
-            // Make sure you log the exception, as it might be swallowed
-            System.err.println("Initial SessionFactory creation failed." + ex);
-            throw new ExceptionInInitializerError(ex);
-        }
-        Ingredient water = new Ingredient("Water", 1.0, 1.0, 1.0, 0.0);
-        Ingredient egg = new Ingredient("Egg", 3.3, 2.2, 1.12, 3.0);
-        Ingredient apple = new Ingredient("Apple", 3.0, 2.0, 1.0, 0.0);
-
-        Session session = sessionFactory.getCurrentSession();
-        session.beginTransaction();
-        session.createQuery("DELETE FROM Ingredient").executeUpdate();
-        session.save(water);
-        session.save(egg);
-        session.save(apple);
-        session.getTransaction().commit();
+        entityManagerFactory = HibernateUtils.getEntityManager();
     }
 }
---
+++ src/main/java/com/jnutrition/backend/HibernateUtils.java
@@ -0,0 +1,34 @@
+package com.jnutrition.backend;
+
+import com.jnutrition.model.Ingredient;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class HibernateUtils {
+
+    private static EntityManagerFactory entityManagerFactory;
+
+    public static EntityManagerFactory getEntityManager() {
+        if(entityManagerFactory == null)
+            createEntityManager();
+        return entityManagerFactory;
+    }
+
+    private static void createEntityManager() {
+        entityManagerFactory = Persistence.createEntityManagerFactory("CRM");
+
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        Ingredient water = new Ingredient("Water", 1.0, 1.0, 1.0, 0.0);
+        Ingredient egg = new Ingredient("Egg", 3.3, 2.2, 1.12, 3.0);
+        Ingredient apple = new Ingredient("Apple", 3.0, 2.0, 1.0, 0.0);
+
+        entityManager.getTransaction().begin();
+        entityManager.createQuery("DELETE FROM Ingredient i").executeUpdate();
+        entityManager.persist(water);
+        entityManager.persist(egg);
+        entityManager.persist(apple);
+        entityManager.getTransaction().commit();
+    }
+}
